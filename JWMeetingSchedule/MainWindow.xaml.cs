a2b5e614033bcaf9b621e59fa58442470bacdf05
Emanuel Frenzel <emanuel.frenzel@gmail.com>
2019-07-30 16:53:23 +0300

    Added StackPanel in MainWindow

--- JWMeetingSchedule/MainWindow.xaml.cs
+++ JWMeetingSchedule/MainWindow.xaml.cs
@@ -16,5 +16,17 @@ namespace JWMeetingSchedule
             peopleList = new PeopleList();
             DataContext = peopleList;
         }
+
+        private void BtnSchedule_Click(object sender, RoutedEventArgs e)
+        {
+            BtnSchedule.IsChecked = true;
+            BtnPersons.IsChecked = false;
+        }
+
+        private void BtnPersons_Click(object sender, RoutedEventArgs e)
+        {
+            BtnPersons.IsChecked = true;
+            BtnSchedule.IsChecked = false;
+        }
     }
 }
